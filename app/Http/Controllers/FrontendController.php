62cc0f1ad321863f69f9f78fb806d467887938fc
zares <sergiozares@gmail.com>
2020-11-13 11:57:28 +0200

    OK

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -12,7 +12,7 @@ class FrontendController extends Controller
      * $request->substances = [1,2,3,5,7]
      *
      * @param   \lluminate\Http\Request $request
-     * @return  \Illuminate\Http\Resources\Json 
+     * @return  \Illuminate\Http\Resources\Json
      */
     public function findProducts(Request $request)
     {
@@ -23,17 +23,20 @@ class FrontendController extends Controller
         }
 
         $products = Product::where('status', 1)
+            // Get the products that do not have substances with the status "0"
             ->whereDoesntHave('substances', function ($query) {
                 $query->where('substances.status', 0);
             })
+            // Trying to get products with five matches
             ->whereHas('substances', function ($query) use ($substances) {
                 $query->whereIn('substances.id', $substances);
             }, 5);
 
-        if ($products->get()->count()) {
+        if ($products->count()) {
             return $products->get();
         }
 
+        // Get the products with at least two matches
         $products->orWhereHas('substances', function ($query) use ($substances) {
                 $query->whereIn('substances.id', $substances);
             }, '>=', 2)
